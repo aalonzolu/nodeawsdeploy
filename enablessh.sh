6e07853de8e8db0bfb117040fcbc0f7b8aa48754
Lexo Alonzo <aalonzolu@gmail.com>
2020-06-24 17:10:09 -0600

    test ssh

--- enablessh.sh
+++ enablessh.sh
@@ -8,7 +8,9 @@ if [ -z "$1" ]
     HOSTNAME=`hostname` ssh-keygen -t rsa -C "$HOSTNAME" -f "$HOME/.ssh/id_rsa" -P "" && cat ~/.ssh/id_rsa.pub
   else
     echo "$@" >~/.ssh/id_rsa
+    echo "Key loaded"
+    chmod 600 ~/.ssh/id_rsa
+    ls -lah ~/.ssh
+    ssh-add ~/.ssh/id_rsa
 fi
-chmod 600 ~/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
 
