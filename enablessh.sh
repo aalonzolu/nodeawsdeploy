1402f6e82763c6741bfea0f5eebec88158a4589c
Lexo Alonzo <aalonzolu@gmail.com>
2020-06-24 17:14:13 -0600

    test ssh

--- enablessh.sh
+++ enablessh.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
 eval $(ssh-agent -s)
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-echo $@;
+echo $1;
+echo "echoed key"
 if [ -z "$1" ]
   then
     echo "No ssh key spesified, generating..."
     HOSTNAME=`hostname` ssh-keygen -t rsa -C "$HOSTNAME" -f "$HOME/.ssh/id_rsa" -P "" && cat ~/.ssh/id_rsa.pub
   else
-    echo "$@" >~/.ssh/id_rsa
+    echo "$1" >~/.ssh/id_rsa
     echo "Key loaded"
     chmod 600 ~/.ssh/id_rsa
     ls -lah ~/.ssh
